031486ca2857393303a5545ac31b28da3ce1006b
Maybaba <sinyunjong@gmail.com>
2024-05-24 10:49:16 +0900

    # rest api - status

--- src/main/java/com/study/springstudy/springmvc/chap05/rest/RestApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/rest/RestApiController.java
@@ -7,11 +7,10 @@ import com.study.springstudy.springmvc.chap04.common.Search;
 import com.study.springstudy.springmvc.chap04.dto.BoardListResponseDto;
 import com.study.springstudy.springmvc.chap04.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -51,6 +50,7 @@ public class RestApiController {
 
     @GetMapping("/board")
     public Map<String, Object> board() {
+
         List<BoardListResponseDto> list = boardService.findList(new Search());
 
         HashMap<String, Object> map = new HashMap<>();
@@ -60,4 +60,50 @@ public class RestApiController {
         return map;
 
     }
+
+    /*
+         RestController에서 리턴타입을 ResponseEntity를 쓰는 이유
+
+         - 클라이언트에게 응답할 때는 메시지 바디안에 들어 있는 데이터도 중요하지만
+         - 상태코드와 헤더정보를 포함해야 한다
+         - 근데 일반 리턴타입은 상태코드와 헤더정보를 전송하기 어렵다
+     */
+
+    @GetMapping("/people")
+    public ResponseEntity<?> people() {
+        Person p1 = new Person(111, "딸기공주", 30);
+        Person p2 = new Person(222, "초코초코", 20);
+        Person p3 = new Person(333, "냠냠냠", 40);
+
+        List<Person> people = List.of(p1, p2, p3);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("my-pet", "cat");
+        headers.add("money", "300");
+
+        return ResponseEntity
+//                .badRequest()
+//                .internalServerError()
+//                .status(404) //응답신호(번호) 커스텀 가능 ㅋㅋ
+                .ok()
+                .headers(headers)
+                .body(people); //담고 싶은 정보
+    }
+    @GetMapping("/bmi")
+    public ResponseEntity<?> bmi (
+            @RequestParam(required = false) Double cm,
+            @RequestParam(required = false) Double kg
+    ) {
+
+        if(cm == null || kg == null) {
+            return ResponseEntity.badRequest().body("키와 몸무게를 입력해주세요");
+        }
+
+        double m = cm / 100;
+        double bmi = kg / (m * m);
+
+        return ResponseEntity.ok().body(bmi);
+
+    }
+
 }
