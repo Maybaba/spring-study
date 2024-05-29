c73b6238c64bf33f098562cc284b9f9c2e84393f
Maybaba <sinyunjong@gmail.com>
2024-05-29 14:26:03 +0900

    # ajax : 댓글 수정, 삭제

--- src/main/resources/static/assets/js/modifyReply.js
+++ src/main/resources/static/assets/js/modifyReply.js
@@ -1,8 +1,8 @@
 
-import {BASE_URL} from "./reply.js";
+import { BASE_URL } from "./reply.js";
 import {fetchInfScrollReplies} from "./getReply.js";
 
-//댓글 수정 비동기 요청 처리 함수
+
 // const fetchModifyReply = async (rno) => {
 //     const res = await fetch(`${BASE_URL}/${rno}`, {
 //         method: 'MODIFY'
@@ -17,7 +17,9 @@ import {fetchInfScrollReplies} from "./getReply.js";
 //     window.scrollTo(0,0); // 삭제 후 페이지 상단으로 이동
 // }
 
+//댓글 수정 비동기 요청 처리 함수
 export function modifyReplyClickEvent() {
+
     document.getElementById('replyData').addEventListener('click', e => {
         e.preventDefault(); //A 태그 삭제
 
@@ -30,8 +32,45 @@ export function modifyReplyClickEvent() {
 
         // modReplyText ID를 가진 textarea의 placeholder 속성에 텍스트를 설정
         document.getElementById('modReplyText').setAttribute('placeholder', placeholderText);
-        //수정요청 class : btn-dark
-        // const rno = e.target.closest('#replyContent').dataset.replyId;
-        // fetchModifyReply(rno);
+
+        //댓글번호 rno 가져오기 -
+        const rno = e.target.closest('#replyContent').dataset.replyId;
+
+        // 모달에 클릭한 댓글번호 달아놓기
+        document.querySelector('.modal').dataset.rno = rno;
         });
+
+    //수정요청 class : btn-dark
+    document.getElementById('replyModBtn').addEventListener('click', e=> {
+
+        fetchReplyModify();
+    });
+
+    async function fetchReplyModify() {
+
+        const payload = {
+            rno: document.querySelector('.modal').dataset.rno,
+            newText: document.getElementById('modReplyText').value,
+            bno: document.getElementById('wrap').dataset.bno
+        }
+
+        const res = await fetch(BASE_URL, {
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        if(!res.ok) {
+            alert('수정 실패');
+        } else {
+
+            //수정 후 모달 닫기 버튼 클릭
+            document.getElementById('modal-close').click();
+
+            window.scrollTo(0,500); //수정 후 위로 이동
+            await fetchInfScrollReplies();
+        }
+    }
 }
