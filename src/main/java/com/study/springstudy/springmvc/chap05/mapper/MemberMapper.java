2f4a4ae5594c7b5dba3a96d12986e855e2dc1448
Maybaba <sinyunjong@gmail.com>
2024-05-29 14:55:09 +0900

    # 회원관리 시스템 만들기 : entity, mapper

---
+++ src/main/java/com/study/springstudy/springmvc/chap05/mapper/MemberMapper.java
@@ -0,0 +1,25 @@
+package com.study.springstudy.springmvc.chap05.mapper;
+
+import com.study.springstudy.springmvc.chap05.entity.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+
+    //회원가입
+    boolean save(Member member);
+
+    //회원정보 개별 조회
+    Member findOne(String account);
+
+    /**
+     *
+     * @param type - 어떤걸 중복검사할지
+     * @param keyword - 중복검사할 실제값
+     * @return - 중복이면 true 아니면 false
+     */
+
+    //중복확인(아이디, 이메일)
+    boolean existById(String type, String keyword);
+
+}
