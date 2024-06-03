17a3d86f7311c0b80fc6be87c0691d63495ce6a5
Maybaba <sinyunjong@gmail.com>
2024-06-03 09:50:12 +0900

    # board delete access authorization control by server

--- src/main/java/com/study/springstudy/springmvc/LoginUtil.java
+++ src/main/java/com/study/springstudy/springmvc/LoginUtil.java
@@ -1,22 +1,73 @@
+//package com.study.springstudy.springmvc;
+//
+//import com.study.springstudy.springmvc.chap05.dto.response.LoginUserInfoDto;
+//import com.study.springstudy.springmvc.chap05.entity.Auth;
+//
+//import javax.servlet.http.HttpSession;
+//
+//
+//public class LoginUtil {
+//    public static final String LOGIN = "login";
+//    //로그인 여부 확인
+//    public static boolean isLoggedIn(HttpSession session) {
+//        return session.getAttribute(LOGIN) != null;
+//    }
+//
+//    // 로그인한 회원의 계정명 얻기
+//    public static String getLoggedInUserAccount(HttpSession session) {
+//        LoginUserInfoDto currentUser
+//                = (LoginUserInfoDto) session.getAttribute(LOGIN);
+//        return  (currentUser != null) ? currentUser.getAccount() : null;
+//    }
+//
+//    public static boolean isAdmin(HttpSession session) {
+//        LoginUserInfoDto loggedInUser = getLoggedInUserAccount(session);
+//        Auth auth = null;
+//        if(isLoggedIn(session)) {
+//        auth = Auth.valueOf(loggedInUser.getAuth());
+//        }
+//        return auth == Auth.ADMIN;
+//    }
+//
+//
+//}
+
+
 package com.study.springstudy.springmvc;
 
 import com.study.springstudy.springmvc.chap05.dto.response.LoginUserInfoDto;
+import com.study.springstudy.springmvc.chap05.entity.Auth;
 
 import javax.servlet.http.HttpSession;
 
-
 public class LoginUtil {
+
     public static final String LOGIN = "login";
-    //로그인 여부 확인
+
+    // 로그인 여부 확인
     public static boolean isLoggedIn(HttpSession session) {
         return session.getAttribute(LOGIN) != null;
     }
 
     // 로그인한 회원의 계정명 얻기
     public static String getLoggedInUserAccount(HttpSession session) {
-        LoginUserInfoDto currentUser
-                = (LoginUserInfoDto) session.getAttribute(LOGIN);
-        return  (currentUser != null) ? currentUser.getAccount() : null;
+        LoginUserInfoDto loggedInUser = getLoggedInUser(session);
+        return loggedInUser != null ? loggedInUser.getAccount() : null;
     }
 
+    public static LoginUserInfoDto getLoggedInUser(HttpSession session) {
+        return (LoginUserInfoDto) session.getAttribute(LOGIN);
+    }
+
+    public static boolean isAdmin(HttpSession session) {
+        LoginUserInfoDto loggedInUser = getLoggedInUser(session);
+        Auth auth = null;
+        if (loggedInUser != null) {
+            auth = Auth.valueOf(loggedInUser.getAuth());
+        }
+        return auth == Auth.ADMIN;
+    }
+    public static boolean isMine(String boardAccount, String loggedInUserAccount) {
+        return boardAccount.equals(loggedInUserAccount);
+    }
 }
--- src/main/java/com/study/springstudy/springmvc/interseptor/BoardInterceptor.java
+++ src/main/java/com/study/springstudy/springmvc/interseptor/BoardInterceptor.java
@@ -1,12 +1,19 @@
 package com.study.springstudy.springmvc.interseptor;
 
-import com.study.springstudy.springmvc.LoginUtil;
+import com.study.springstudy.springmvc.chap04.entity.Board;
+import com.study.springstudy.springmvc.chap04.mapper.BoardMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import static com.study.springstudy.springmvc.LoginUtil.*;
 
 //prehandle 구현해서
 //로그인을 안하면 글쓰기, 글수정, 글삭제 요청을 거부할 것
@@ -14,22 +21,55 @@ import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class BoardInterceptor implements HandlerInterceptor {
+
+    private final BoardMapper boardMapper;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
 
         //로그인을 했다면
         //클라이언트 측에 저장된 세션을 가져와서
         //작업하던 곳으로 돌아간다.
-        if (!LoginUtil.isLoggedIn(request.getSession())) {
+        HttpSession session = request.getSession();
 
-            String redirectUri = request.getRequestURI();
+        //리다이렉션 요청 URI
+        String redirectUri = request.getRequestURI();
 
-            log.info("origin: {}", redirectUri);
-            response.sendRedirect("/members/sign-in?message=login-required&redirect=" + redirectUri);
+        if (!isLoggedIn(request.getSession())) {  // 로그인을 안 했을 때
+            String encodedRedirectUri = URLEncoder.encode(redirectUri, StandardCharsets.UTF_8.toString());
+//            log.info("origin: {}", redirectUri);
+            request.getSession().setAttribute("redirect", encodedRedirectUri);  // 세션에 리다이렉트 URI 저장
+            response.sendRedirect("/members/sign-in?message=login-required");
             return false;
         }
+
+        //삭제요청인가에 대한 관리자인가?
+        if(isAdmin(session)) {
+            return true; //네, 통과
+        }
+        //내가 쓴 글인지 아닌지 확인하기
+        if(redirectUri.equals("/board/delete")) {
+            //내가 쓴 글이 아닌지?
+            //현재 삭제하려는 글의 글쓴이 계정명과
+            // DB에서 bno 조회해보면 됨 -> 글번호 구하기
+            String loggedInUserAccount = getLoggedInUserAccount(session);
+
+            int bno = Integer.parseInt(request.getParameter("bno"));
+            Board board = boardMapper.findOne(bno);
+            String boardAccount = board.getAccount();
+
+            //현재 로그인한 회원의 계정명을 구해서
+
+            //대조해보는 작업이 필요함
+            if(!isMine(boardAccount, loggedInUserAccount) ) {
+                response.setStatus(403); //권한이 없음 : forbidden
+                response.sendRedirect("/access-deny?message=authorization");
+                return false;
+            }
+        }
+
         return true;
+
     }
 }
